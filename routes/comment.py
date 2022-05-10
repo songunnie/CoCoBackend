a647eba15116583041d0880e063351ceec8e2621
rimsong <kariber21@naver.com>
2022-05-11 00:38:27 +0900

    Feat: Create deleting comment api

--- routes/comment.py
+++ routes/comment.py
@@ -69,3 +69,25 @@ def get_comment(post_id):
         'status': STATUS_MESSAGE['SUCCESS'],
         'comment': comment
     }))), STATUS_CODE['SUCCESS']
+
+
+@bp.route('/<comment_id>', methods=['DELETE'])
+def delete_comment(comment_id):
+    token = request.cookies.get('token')
+    payload = decode_token(token, current_app.jwt_secret_key, 'HS256')
+
+    if payload == None:
+        return jsonify({
+            'status': STATUS_MESSAGE['INVALID_TOKEN']
+        }), STATUS_CODE['INVALID_TOKEN']
+
+    if current_app.db.users.find_one({'id': payload['id']}) == None:
+        return jsonify({
+            'status': STATUS_MESSAGE['INVALID_TOKEN']
+        }), STATUS_CODE['INVALID_TOKEN']
+
+    current_app.db.comments.delete_one({'_id': ObjectId(comment_id)})
+
+    return jsonify({
+        'status': STATUS_MESSAGE['SUCCESS']
+    }), STATUS_CODE['SUCCESS']
