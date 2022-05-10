65e5b166bbede2221d4dcc4180c88cffdd455e9e
rimsong <kariber21@naver.com>
2022-05-10 23:13:57 +0900

    Feat: Create getting comment api

--- routes/comment.py
+++ routes/comment.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, current_app, jsonify, request
+from bson.objectid import ObjectId
+
+from flask import Blueprint, current_app, jsonify, request, json
 
 from presets.status import STATUS_CODE, STATUS_MESSAGE
 
@@ -8,6 +10,7 @@ from utils.token import decode_token
 
 # Flask Blueprint 생성
 bp = Blueprint('comment', __name__, url_prefix='/comment')
+
 
 @bp.route('/<post_id>', methods=['POST'])
 def write_comment(post_id):
@@ -43,3 +46,26 @@ def write_comment(post_id):
     return jsonify({
         'status': STATUS_MESSAGE['SUCCESS']
     }), STATUS_CODE['SUCCESS']
+
+
+@bp.route('/<post_id>', methods=['GET'])
+def get_comment(post_id):
+    token = request.cookies.get('token')
+    payload = decode_token(token, current_app.jwt_secret_key, 'HS256')
+
+    if payload == None:
+        return jsonify({
+            'status': STATUS_MESSAGE['INVALID_TOKEN']
+        }), STATUS_CODE['INVALID_TOKEN']
+
+    if current_app.db.users.find_one({'id': payload['id']}) == None:
+        return jsonify({
+            'status': STATUS_MESSAGE['INVALID_TOKEN']
+        }), STATUS_CODE['INVALID_TOKEN']
+
+    comment = current_app.db.comments.find_one({'_id': ObjectId(post_id)}, {'_id': False})
+
+    return jsonify(**json.loads(json.htmlsafe_dumps({
+        'status': STATUS_MESSAGE['SUCCESS'],
+        'comment': comment
+    }))), STATUS_CODE['SUCCESS']
