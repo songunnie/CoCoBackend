3b21a3f4c91c4e6c63bd961b6366eb0499c15061
장세리 <srlim0226@gmail.com>
2022-05-10 22:46:49 +0900

    Feat: Create listing message api

--- routes/message.py
+++ routes/message.py
@@ -119,3 +119,31 @@ def delete_message(message_id):
     return jsonify({
         'status': STATUS_MESSAGE['SUCCESS']
     }), STATUS_CODE['SUCCESS']
+
+
+# 쪽지 목록 라우터
+@bp.route('/list', methods=['GET'])
+def get_message_list():
+    token = request.cookies.get('token')
+    payload = decode_token(token, current_app.jwt_secret_key, 'HS256')
+
+    if payload == None:
+        return jsonify({
+            'status': STATUS_MESSAGE['INVALID_TOKEN']
+        }), STATUS_CODE['INVALID_TOKEN']
+
+    if current_app.db.users.find_one({'id': payload['id']}) == None:
+        return jsonify({
+            'status': STATUS_MESSAGE['INVALID_TOKEN']
+        }), STATUS_CODE['INVALID_TOKEN']
+
+    message_list = list(
+        current_app.db.messages.find({'receiver_user_id': payload['id']}, {'content': False}).sort('_id', -1))
+
+    for index in range(len(message_list)):
+        message_list[index]['_id'] = str(message_list[index]['_id'])
+
+    return jsonify(**json.loads(json.htmlsafe_dumps({
+        'status': STATUS_MESSAGE['SUCCESS'],
+        'data': message_list
+    }))), STATUS_CODE['SUCCESS']
