d297217e1b2d62dcc643b28eb46ab68b48c55a07
kangwongu <kwg0527@naver.com>
2022-07-13 20:56:57 +0900

    Refactor: Modify post response status

--- src/main/java/com/igocst/coco/controller/PostController.java
+++ src/main/java/com/igocst/coco/controller/PostController.java
@@ -1,11 +1,13 @@
 package com.igocst.coco.controller;
 
 
+import com.igocst.coco.domain.Member;
 import com.igocst.coco.domain.MemberRole;
 import com.igocst.coco.dto.post.*;
 import com.igocst.coco.security.MemberDetails;
 import com.igocst.coco.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,33 +23,33 @@ public class PostController {
 
     // 게시글 생성
     @PostMapping("/post")
-    public PostSaveResponseDto createPost(@RequestBody PostSaveRequestDto requestDto,
-                                          @AuthenticationPrincipal MemberDetails memberDetails) {
+    public ResponseEntity<PostSaveResponseDto> createPost(@RequestBody PostSaveRequestDto requestDto,
+                                                         @AuthenticationPrincipal MemberDetails memberDetails) {
         return postService.createPost(requestDto, memberDetails);
     }
 
     // 게시글 상세 조회
     @GetMapping("/post/{postId}")
-    public PostReadResponseDto readPost(@PathVariable Long postId,
+    public ResponseEntity<PostReadResponseDto> readPost(@PathVariable Long postId,
                                         @AuthenticationPrincipal MemberDetails memberDetails) {
         return postService.readPost(postId, memberDetails);
     }
 
     // 게시글 전체 목록 조회
     @GetMapping("/post/list")
-    public List<PostReadResponseDto> readPostList() {
+    public ResponseEntity<List<PostReadResponseDto>> readPostList() {
         return postService.readPostList();
     }
 
     // 모집 중인 게시글 목록 조회
     @GetMapping("/post/recruit/list")
-    public List<PostReadResponseDto> readRecruitingPostList() {
+    public ResponseEntity<List<PostReadResponseDto>> readRecruitingPostList() {
         return postService.readRecruitingPostList();
     }
 
     // 게시글 수정
     @PutMapping("/post/{postId}")
-    public PostUpdateResponseDto updatePost(@PathVariable Long postId,
+    public ResponseEntity<PostUpdateResponseDto> updatePost(@PathVariable Long postId,
                                             @RequestBody PostUpdateRequestDto requestDto,
                                             @AuthenticationPrincipal MemberDetails memberDetails) {
         return postService.updatePost(postId, requestDto, memberDetails);
@@ -55,7 +57,7 @@ public class PostController {
 
     // 게시글 삭제
     @DeleteMapping("/post/{postId}")
-    public PostDeleteResponseDto deletePost(@PathVariable Long postId,
+    public ResponseEntity<PostDeleteResponseDto> deletePost(@PathVariable Long postId,
                                             @AuthenticationPrincipal MemberDetails memberDetails) {
         return postService.deletePost(postId, memberDetails);
     }
@@ -63,7 +65,8 @@ public class PostController {
     // 관리자, 게시글 삭제
     @Secured(MemberRole.Authority.ADMIN)
     @DeleteMapping("/admin/post/{postId}")
-    public PostDeleteResponseDto adminDeletePost(@PathVariable Long postId) {
-        return postService.adminDeletePost(postId);
+    public ResponseEntity<PostDeleteResponseDto> adminDeletePost(@PathVariable Long postId,
+                                                 @AuthenticationPrincipal MemberDetails memberDetails) {
+        return postService.adminDeletePost(postId, memberDetails);
     }
 }
