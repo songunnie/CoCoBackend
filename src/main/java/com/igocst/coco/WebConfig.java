e3e4284972f2a03448d9e15de3fc1379c2bf8945
kangwongu <kwg0527@naver.com>
2022-07-28 10:04:44 +0900

    Fix: set cookie domain & cors in Deployment Environment

--- src/main/java/com/igocst/coco/WebConfig.java
+++ src/main/java/com/igocst/coco/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("*")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("https://www.cocoding.xyz/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
